9606f1efec3d4680afa60a4629f304673c7b55fa
minsu <minsur0919@gmail.com>
2025-03-19 15:11:19 +0900

    매핑 수정 중 푸쉬 테스트

--- src/main/java/ezmarket/BoardController.java
+++ src/main/java/ezmarket/BoardController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 
 @RestController
+//@RequestMapping("/api/board") 푸쉬테스트
 public class BoardController {
 	
 	@Autowired
