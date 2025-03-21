89b71fac3cab522b798a5cb1a9698faab406d720
hong Kil Dong <sion9199@gmail.com>
2025-03-21 10:17:04 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontendController {
 
-    @RequestMapping("/**/{path:[^\\.]+}")
+    @RequestMapping(value = { "/", "/**/{[path:[^\\.]*}"} )
     public String forward() {
         return "forward:/index.html";
     }
 }
+
