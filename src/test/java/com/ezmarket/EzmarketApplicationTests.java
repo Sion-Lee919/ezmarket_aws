6d66b093b2bb9c1767a10056b47e3e9ffe1347fe
hong Kil Dong <sion9199@gmail.com>
2025-03-21 10:32:47 +0900

    수정

--- src/test/java/com/ezmarket/EzmarketApplicationTests.java
+++ src/test/java/com/ezmarket/EzmarketApplicationTests.java
@@ -1,9 +1,13 @@
 package com.ezmarket;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import ezmarket.FrontendController;
+
 @SpringBootTest
+@ImportAutoConfiguration(exclude = FrontendController.class)
 class EzmarketApplicationTests {
 
 	@Test
