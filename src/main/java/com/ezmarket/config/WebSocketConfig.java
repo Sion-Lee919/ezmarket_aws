d55c130fe817219072fe5d4c68b2f128eec7a71b
minsu <minsur0919@gmail.com>
2025-03-21 17:19:47 +0900

    체크용 로그 추가

--- src/main/java/com/ezmarket/config/WebSocketConfig.java
+++ src/main/java/com/ezmarket/config/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package com.ezmarket.config;
 
+import java.io.Console;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -14,15 +16,22 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+    	System.out.println("endpoint 설정");
         registry.addEndpoint("/ws")
                 //.setAllowedOrigins("*")
         		.setAllowedOriginPatterns("*/**");
+    	System.out.println("endpoint 설정종료");
+
         		
     }
     
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+    	System.out.println("/sub 체크");
         registry.enableSimpleBroker("/sub");
+        System.out.println("/sub 체크종료");
+        System.out.println("/pub 종착지 체크");
         registry.setApplicationDestinationPrefixes("/pub");
+        System.out.println("/pub 종착지 체크종료");
     }
 }
