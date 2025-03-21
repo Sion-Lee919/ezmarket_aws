80301d66daaa82e39ea8a6353bb3f89978c680b6
wendys1205 <wendys1205@naver.com>
2025-03-21 13:56:45 +0900

    판매자 주문 내역 수정

--- src/main/java/ezmarket/OrderMapper.java
+++ src/main/java/ezmarket/OrderMapper.java
@@ -7,6 +7,8 @@ import java.util.List;
 @Mapper
 public interface OrderMapper {
     void insertOrder(OrderDTO order);
+    
+    List<OrderDTO> getAllOrders();
 
     List<OrderDTO> getOrdersByMemberId(@Param("memberId") int memberId);
 
@@ -15,6 +17,8 @@ public interface OrderMapper {
     void updateOrderStatus(@Param("orderId") int orderId, @Param("status") String status);
     
     OrderDTO getOrderByMemberIdAndOrderId(@Param("memberId") int memberId, @Param("orderId") int orderId);
+    
+    OrderDTO getOrderByOrderId(@Param("orderId") int orderId);
     
     //Member Part
     int getOrderCountByStatus(@Param("status") String status, @Param("member_id") int member_id);
