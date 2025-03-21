921a6e976bb4553f7b21a38104433a1314e1f65b
hong Kil Dong <sion9199@gmail.com>
2025-03-21 10:04:17 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -1,13 +1,14 @@
 package ezmarket;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FrontendController {
 
-    @GetMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    @RequestMapping({"/", "/{path:[^\\.]*}"})
     public String forwardToIndex() {
         return "forward:/index.html";
     }
+    
 }
