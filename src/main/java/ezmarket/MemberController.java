4c58f0da05909ea1de647568ecc38f2e2b3ccf79
Hyeon5 <hyeon5@github.com>
2025-03-21 13:35:52 +0900

    판매자 관리 수정

--- src/main/java/ezmarket/MemberController.java
+++ src/main/java/ezmarket/MemberController.java
@@ -3,7 +3,9 @@ package ezmarket;
 
 import java.util.ArrayList;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -283,14 +285,16 @@ public class MemberController {
 
 	        try {
 	            String brandLogoFileName = brand_id + "_" + brand_number + "_" + brandLogoFile.getOriginalFilename();
+	            String brandlogo_url = brandLogoFile.getOriginalFilename();
 	            String path_logo = "/home/" + System.getProperty("user.name") + "/mydir/ezmarketupload/" + "brandlogo/";
-	            String brandlogo_url = path_logo + brandLogoFileName;
-	            saveFile(brandLogoFile, brandlogo_url);
+	            String brandlogo_full_url = path_logo + brandLogoFileName;
+	            saveFile(brandLogoFile, brandlogo_full_url);
 	            
 	            String brandLicenseFileName = brand_id + "_" + brand_number + "_" + brandLicenseFile.getOriginalFilename();
+	            String brandlicense_url = brandLicenseFile.getOriginalFilename();
 	            String path_license = "/home/" + System.getProperty("user.name") + "/mydir/ezmarketupload/" + "brandlicense/";
-	            String brandlicense_url = path_license + brandLicenseFileName;
-	            saveFile(brandLicenseFile, brandlicense_url);
+	            String brandlicense_full_url = path_license + brandLicenseFileName;
+	            saveFile(brandLicenseFile, brandlicense_full_url);
 	            
 	            MemberDTO dto = new MemberDTO();
 	            dto.setBrand_id(brand_id);
@@ -312,6 +316,30 @@ public class MemberController {
 		        File destinationFile = new File(path);
 		        destinationFile.getParentFile().mkdirs();
 		        file.transferTo(destinationFile);
+		    }
+		    
+		    // 파일 다운로드
+		    @GetMapping("/downloadFile")
+		    public void downloadFile(@RequestParam String filename, HttpServletResponse response) throws IOException {
+		        String path = "/home/" + System.getProperty("user.name") + "mydir/ezmarketupload/brandlicense/";  // 파일 경로 설정
+		        File file = new File(path + filename);
+
+		        if (!file.exists()) {
+		            response.sendError(HttpServletResponse.SC_NOT_FOUND, "파일이 존재하지 않습니다.");
+		            return;
+		        }
+
+		        response.setContentType("application/pdf"); 
+		        response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+
+		        try (FileInputStream inputStream = new FileInputStream(file);
+		             OutputStream outputStream = response.getOutputStream()) {
+		            byte[] buffer = new byte[1024];
+		            int bytesRead;
+		            while ((bytesRead = inputStream.read(buffer)) != -1) {
+		                outputStream.write(buffer, 0, bytesRead);
+		            }
+		        }
 		    }
 		
 		//중복 확인 - 사업자번호
