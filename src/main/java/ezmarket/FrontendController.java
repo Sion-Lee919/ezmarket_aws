43432ffb632c57ccc45ec55a4f78e118d16f8481
hong Kil Dong <sion9199@gmail.com>
2025-03-22 10:29:55 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = {"/{path:^(?!ws$).*$}", "/{path:^(?!ws$).*$}/**"})
+    @RequestMapping(value = {
+            "/", 
+            "/{x:^(?!ws$|api$|login$|chat$).*$}/**" // ws, api, login, chat 제외
+    })
     public String forward() {
         return "forward:/index.html";
     }
 }
+
 
