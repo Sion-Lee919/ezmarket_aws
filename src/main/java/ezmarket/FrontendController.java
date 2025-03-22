fdca54baa80469912adee9d8fae6032123b732f6
hong Kil Dong <sion9199@gmail.com>
2025-03-22 12:11:43 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -1,25 +1,25 @@
-package ezmarket;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-@Controller
-public class FrontendController {
-
-	@RequestMapping(value = "/**/{path:[^\\.]*}")
-    public String redirect(HttpServletRequest request) {
-        String uri = request.getRequestURI();
-
-        // WebSocket 요청은 무시하고 포워드하지 않음
-        if (uri.startsWith("/ws")) {
-            return null; // ViewResolver에도 걸리지 않게
-        }
-
-        return "forward:/index.html";
-    }
-}
+//package ezmarket;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//
+//@Controller
+//public class FrontendController {
+//
+//	@RequestMapping(value = "/**/{path:[^\\.]*}")
+//    public String redirect(HttpServletRequest request) {
+//        String uri = request.getRequestURI();
+//
+//        // WebSocket 요청은 무시하고 포워드하지 않음
+//        if (uri.startsWith("/ws")) {
+//            return null; // ViewResolver에도 걸리지 않게
+//        }
+//
+//        return "forward:/index.html";
+//    }
+//}
 
 
 
