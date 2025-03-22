94e22ddff18c46bcf66cae3493a6c12e9dac6956
hong Kil Dong <sion9199@gmail.com>
2025-03-22 13:12:35 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -1,22 +1,22 @@
-package ezmarket;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-@Controller
-public class FrontendController {
-
-    @RequestMapping(value = "/**/{path:[^\\.]*}")
-    public Object redirect(HttpServletRequest request) {
-        String uri = request.getRequestURI();
-
-        if (uri.startsWith("/ws") || uri.startsWith("/api") || uri.startsWith("/static") || uri.startsWith("/resources")) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return "forward:/index.html";
-    }
-}
+//package ezmarket;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//
+//@Controller
+//public class FrontendController {
+//
+//    @RequestMapping(value = "/**/{path:[^\\.]*}")
+//    public Object redirect(HttpServletRequest request) {
+//        String uri = request.getRequestURI();
+//
+//        if (uri.startsWith("/ws") || uri.startsWith("/api") || uri.startsWith("/static") || uri.startsWith("/resources")) {
+//            return ResponseEntity.notFound().build();
+//        }
+//
+//        return "forward:/index.html";
+//    }
+//}
