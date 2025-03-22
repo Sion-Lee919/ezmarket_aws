09bfe939dc47d35738a58f588155e4016af2430d
hong Kil Dong <sion9199@gmail.com>
2025-03-22 11:22:12 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -8,14 +8,18 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class FrontendController {
 
-	@RequestMapping("/**/{path:[^\\.]+}")
-	public String redirect(HttpServletRequest request) {
-	    String uri = request.getRequestURI();
-	    if (uri.startsWith("/ws")) {
-	        return null; // WebSocket 경로는 무시
-	    }
-	    return "forward:/index.html";
-	}
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirect(HttpServletRequest request) {
+        String uri = request.getRequestURI();
 
+        // WebSocket 요청은 무시하고 포워드하지 않음
+        if (uri.startsWith("/ws")) {
+            return null; // ViewResolver에도 걸리지 않게
+        }
+
+        return "forward:/index.html";
+    }
 }
+
+
 
