1625d9650f133d1078592af640f8179973d42368
hong Kil Dong <sion9199@gmail.com>
2025-03-22 11:06:01 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontendController {
 
-    // 정규식으로 .으로 끝나지 않고 ws로 시작하지 않는 모든 경로 매칭
-    @RequestMapping(value = { "/{path:^(?!ws$)[^\\.]*}", "/**/{path:^(?!ws).*}" })
+    // 정규식에서 WebSocket 경로 제외하고, .으로 끝나는 경로도 제외
+    @RequestMapping(value = {"/{path:[^\\.]+}", "/**/{path:^(?!ws)(?!.*\\..*).*$}"})
     public String forward() {
         return "forward:/index.html";
     }
 }
+
