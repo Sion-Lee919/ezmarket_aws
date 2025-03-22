9258fdd43da5efe763411b184b5d6ce3289ada65
hong Kil Dong <sion9199@gmail.com>
2025-03-22 10:55:40 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -1,13 +1,14 @@
-//package ezmarket;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class FrontendController {
-//
-//    @RequestMapping("/**/{path:[^\\.]+}")
-//    public String forward() {
-//        return "forward:/index.html";
-//    }
-//}
+package ezmarket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FrontendController {
+
+    // 정규식으로 .으로 끝나지 않고 ws로 시작하지 않는 모든 경로 매칭
+    @RequestMapping(value = { "/{path:^(?!ws$)[^\\.]*}", "/**/{path:^(?!ws).*}" })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
