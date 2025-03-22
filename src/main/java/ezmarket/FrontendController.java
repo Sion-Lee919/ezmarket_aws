63762aebda7a869fbc74ac47a1446f23a32a2faf
hong Kil Dong <sion9199@gmail.com>
2025-03-22 11:09:53 +0900

    수정

--- src/main/java/ezmarket/FrontendController.java
+++ src/main/java/ezmarket/FrontendController.java
@@ -3,11 +3,19 @@ package ezmarket;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = { "/", "/{path:^(?!api|ws|static|error).*$}", "/**/{path:^(?!.*\\..*$).*$}" })
-    public String forward() {
+    @RequestMapping(value = {
+            "/", 
+            "/{path:[^\\.]*}", 
+            "/**/{path:[^\\.]*}"
+    })
+    public String forward(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        if (uri.startsWith("/ws")) return null; // WebSocket은 제외
         return "forward:/index.html";
     }
 }
