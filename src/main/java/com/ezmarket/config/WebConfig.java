96ba1d5c6a87516fc0f682b7f2dec761dbe47218
hong Kil Dong <sion9199@gmail.com>
2025-03-22 09:31:32 +0900

    수정

--- src/main/java/com/ezmarket/config/WebConfig.java
+++ src/main/java/com/ezmarket/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.ezmarket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
         // 정적 리소스 처리 경로 제한
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // "/" 요청 시 index.html 반환
+        registry.addViewController("/").setViewName("forward:/index.html");
     }
 }
